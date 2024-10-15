b19584ae82f79c23cc06b74e70c7acfec902da9c
Hive Fagammon <hive@fagammon.edu.br>
2024-10-14 23:05:27 -0300

    finalização da pagina principal ajudete de melhorias em algunas pontos

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,1 +1,19 @@
+"use strict"
+
 import 'bootstrap';
+
+const hamburgerMenu = document.querySelector('#hamburger')
+var hamburgerMenuClicked = 0
+console.log(hamburgerMenu)
+
+hamburgerMenu.addEventListener('click', event => {
+
+  if(hamburgerMenuClicked === 1) {
+      hamburgerMenuClicked = 0 
+      hamburgerMenu.src =  'https://www.svgrepo.com/show/493683/hamburger-2-menu-mobile.svg'
+  } else {
+    hamburgerMenuClicked = 1
+    hamburgerMenu.src = "https://www.svgrepo.com/show/522506/close.svg"
+  }
+
+})
