4bb64b6710acf68c0c3f31fd154f5ad543b31b6a
Enzo Athayde <enzo.athayde.dev@gmail.com>
2024-10-16 15:51:17 -0300

    Implementação do formulário de LEADS, integrado com backend no Google Apps Script, que armazena dados do formulário no Google Planilhas.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,18 +2,51 @@
 
 import 'bootstrap';
 
-const hamburgerMenu = document.querySelector('#hamburger')
-var hamburgerMenuClicked = 0
-console.log(hamburgerMenu)
+function collapseMenu() {
 
-hamburgerMenu.addEventListener('click', event => {
+  const hamburgerMenu = document.querySelector('#hamburger')
+  let hamburgerMenuOpened = false
 
-  if(hamburgerMenuClicked === 1) {
-      hamburgerMenuClicked = 0 
-      hamburgerMenu.src =  'https://www.svgrepo.com/show/493683/hamburger-2-menu-mobile.svg'
-  } else {
-    hamburgerMenuClicked = 1
-    hamburgerMenu.src = "https://www.svgrepo.com/show/522506/close.svg"
-  }
+  hamburgerMenu.addEventListener('click', event => {
 
-})
+    hamburgerMenuOpened = !hamburgerMenuOpened;
+    hamburgerMenu.src = hamburgerMenuOpened ? "https://www.svgrepo.com/show/522506/close.svg" : 'https://www.svgrepo.com/show/493683/hamburger-2-menu-mobile.svg';
+
+  })
+
+}
+
+function newLead() {
+
+  document.getElementById("lead").addEventListener("submit", function(e) {
+ 
+    e.preventDefault();
+    const backendURL = 'https://script.google.com/macros/s/AKfycbyUi8Rgk2ACa2ep9S4jHRJ65XkBHTDhRyl1NvuIzRpmPk6WYa4dEdDROds_bnVPtR1yJg/exec'
+    const formData = new FormData(this);
+    
+    fetch(backendURL, {
+        method: "POST",
+        body: formData,
+    })  
+    .then(response => response.json())
+    .then(result => {
+        alert("Dados enviados com sucesso!");
+        console.log(result);  
+    })
+    .catch(error => {
+        console.error("Erro ao enviar dados:", error);
+    });
+  });
+
+}
+
+
+// Implementar quando a parte do formulário estiver pronta.
+// function formatarCep() {
+//   fieldCEP = document.querySelector()
+// }
+
+
+collapseMenu()
+newLead()
+// formatarCep()
