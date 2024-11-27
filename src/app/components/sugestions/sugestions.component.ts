adbca0211d0cb736d6358c4c31f3aa6e3ae452d1
Enzo Athayde <enzo.athayde.dev@gmail.com>
2024-11-27 17:05:14 -0300

    configurações de estilo e tentativa de conserto para o formulario apps script

--- src/app/components/sugestions/sugestions.component.ts
+++ src/app/components/sugestions/sugestions.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { SugestionForm } from './sugestionForm';
 import { FormsModule } from '@angular/forms';
+import axios from 'axios'
 
 @Component({
   selector: 'app-sugestions',
@@ -9,6 +10,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './sugestions.component.scss',
 })
 export class SugestionsComponent {
+
   newSugestion: SugestionForm = {
     name: '',
     email: '',
@@ -22,6 +24,28 @@ export class SugestionsComponent {
   };
 
   submit() {
-    console.log('SugestionsComponent submit');
+
+    const backendURL = 'https://script.google.com/macros/s/AKfycbyUi8Rgk2ACa2ep9S4jHRJ65XkBHTDhRyl1NvuIzRpmPk6WYa4dEdDROds_bnVPtR1yJg/exec'
+
+
+    if (
+      this.newSugestion.name &&
+      this.newSugestion.email &&
+      this.newSugestion.cep &&
+      this.newSugestion.street &&
+      this.newSugestion.neighborhood &&
+      this.newSugestion.city &&
+      this.newSugestion.state &&
+      this.newSugestion.phone &&
+      this.newSugestion.message
+    ) {
+
+      axios.post(backendURL, {name: this.newSugestion.name, email: this.newSugestion.email, cep: this.newSugestion.cep, street: this.newSugestion.street, neighborhood: this.newSugestion.neighborhood, city: this.newSugestion.city, state: this.newSugestion.state, message: this.newSugestion.message})
+
+      console.log("Todos os campos estão preenchidos!");
+    } else {
+      window.alert("Há campos vazios.");
+    }
+    
   }
 }
